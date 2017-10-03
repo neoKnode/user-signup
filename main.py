e3e5161551bcaf7f4c097214cba84bfd07aab76f
NEO <31035762+neoKnode@users.noreply.github.com>
2017-10-02 23:43:06 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 1px;
+            }
+
+        </style>
+    </head>
+    <body>
+         <form method="POST">
+            <label for="username">Username</label>
+            <input id="username" type="text" name="userfield" value="" />
+            <br>
+            <br>
+            <br>
+            <input type="submit" />
+        </form>
+    </body>
+</html>
+"""
+
+@app.route("/")
+def index():
+    return form
+
+app.run()
