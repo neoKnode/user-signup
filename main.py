e01345acc0df050ce745bf948cb0c3cea0cc11e7
Bob <bob@Bobs-MacBook-Pro.local>
2017-10-09 06:03:44 -0500

    Finished User-Signup

--- main.py
+++ main.py
@@ -1,25 +1,55 @@
 
-from flask import Flask, request, redirect
+from flask import Flask, request, render_template
 import cgi
-import os
-import jinja2
 
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/")
+
+@app.route('/')
 def index():
-    template = jinja_env.get_template('index.html')
-    return template.render()
+    return render_template('signup_form.html')
 
-@app.route("/welcome", methods=['POST'])
-def welcome():
-    first_name = request.form['userfield']
-    template = jinja_env.get_template('welcome.html')
-    return template.args.get(name=userfield)
 
+@app.route('/', methods=['POST'])
+def validate_user():
+
+    username = request.form['username']
+    password = request.form['password']
+    verify_password = request.form['verify_password']
+    email = request.form['email']
+
+    username_error = ''
+    password_error = ''
+    verify_error = ''
+    email_error = ''
+
+    if len(username) < 3 or len(username) > 20 or ' ' in username:
+        username_error = 'Username too short/long'
+
+
+    if len(password) < 3 or len(password) > 20 or ' ' in password:
+        password_error = 'Password too short/long'
+
+    if verify_password == '':
+        verify_error = 'Please retype your password'
+
+    if password != verify_password:
+        verify_error = 'Passwords do not match!'
+
+    if email != '' and len(email) < 3 or len(email) > 20:
+        email_error = 'Email too short/long'
+    
+    elif email != '' and ("@" not in email) or ("." not in email):
+        email_error = 'Please enter a valid email'
+
+    if not username_error and not password_error and not verify_error and not email_error:
+        return render_template("welcome.html", username=username)
+    else:
+        return render_template('signup_form.html', username=username,
+            email=email, username_error=username_error, 
+            password_error=password_error, verify_error=verify_error, 
+            email_error=email_error)
 
 app.run()
